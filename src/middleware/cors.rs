443b99a0e928de476e7d5601a26686b1f3b6378e
necessarylion <aj.zinkyaw@gmail.com>
2024-06-17 12:42:02 +0700

    modify cors config to allow multiple origins

--- src/middleware/cors.rs
+++ src/middleware/cors.rs
@@ -1,9 +1,31 @@
 use actix_cors::Cors;
-use actix_web::http;
+use actix_http::{header, Method};
+
+struct CorsConfig {
+  pub headers: Vec<header::HeaderName>,
+  pub methods: Vec<Method>,
+  pub origins: Vec<String>,
+}
 
 pub fn get_cors_config() -> Cors {
+  // cors configuration
+  let cors_config = CorsConfig {
+    headers: vec![header::AUTHORIZATION],
+    methods: vec![
+      Method::GET,
+      Method::POST,
+      Method::PUT,
+      Method::DELETE,
+      Method::HEAD,
+      Method::OPTIONS,
+    ],
+    origins: vec![String::from("*")],
+  };
+
   return Cors::default()
-    .allowed_origin("*")
-    .allowed_methods(vec!["GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"])
-    .allowed_headers(vec![http::header::AUTHORIZATION]);
+    .allowed_origin_fn(move |origin, _| {
+      return cors_config.origins.iter().any(|o| o == "*" || o == origin);
+    })
+    .allowed_methods(cors_config.methods)
+    .allowed_headers(cors_config.headers);
 }
