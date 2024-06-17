3ce5d21c997b8ef9fbbc61938e775625f06fab86
necessarylion <aj.zinkyaw@gmail.com>
2024-06-17 12:01:52 +0700

    rename myError to IError and add not found error handler

--- src/controllers/home_controller.rs
+++ src/controllers/home_controller.rs
@@ -1,19 +1,19 @@
-use actix_web::{get, web, HttpResponse, Responder};
+use actix_web::{get, web::Json, HttpResponse, Responder};
 use serde_json::json;
 
-use crate::{errors::error_handler::MyError, requests::info_request::InfoRequest};
+use crate::{errors::handler::IError, requests::info_request::InfoRequest};
 use validator::Validate;
 
 #[get("/")]
-pub async fn home(info: web::Json<InfoRequest>) -> Result<impl Responder, MyError> {
-  info.validate().map_err(MyError::ValidationError)?;
+pub async fn home(info: Json<InfoRequest>) -> Result<impl Responder, IError> {
+  info.validate().map_err(IError::ValidationError)?;
 
   let obj = json!({
     "name": info.name,
     "email": info.email
   });
 
-  Ok(web::Json(obj))
+  Ok(Json(obj))
 }
 
 #[get("/api/post")]
---
+++ src/errors/handler.rs
@@ -0,0 +1,45 @@
+use actix_http::StatusCode;
+use actix_web::{HttpResponse, ResponseError};
+use derive_more::Display;
+use serde_json::json;
+use validator::ValidationErrors;
+
+#[derive(Debug, Display)]
+pub enum IError {
+  ValidationError(ValidationErrors),
+  ServerError(String),
+  NotFoundError(String),
+}
+
+// Implement ResponseError trait, so that it is able to use as response
+// in services
+impl ResponseError for IError {
+  fn error_response(&self) -> HttpResponse {
+    match &self {
+      IError::ValidationError(e) => handle_validation_error(&e),
+      IError::ServerError(msg) => handle_server_error(&msg),
+      IError::NotFoundError(msg) => handle_not_found_error(&msg),
+    }
+  }
+}
+
+// Handle validation errors
+// and return as json
+fn handle_validation_error(e: &ValidationErrors) -> HttpResponse {
+  HttpResponse::BadRequest()
+    .status(StatusCode::UNPROCESSABLE_ENTITY)
+    .json(e)
+}
+// Handle server errors
+fn handle_server_error(msg: &String) -> HttpResponse {
+  HttpResponse::BadRequest()
+    .status(StatusCode::INTERNAL_SERVER_ERROR)
+    .json(json!({ "message": msg }))
+}
+
+// handle 404 not found error
+fn handle_not_found_error(msg: &String) -> HttpResponse {
+  HttpResponse::BadRequest()
+    .status(StatusCode::NOT_FOUND)
+    .json(json!({ "message": msg }))
+}
